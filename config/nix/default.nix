603c1979111dc2ef7125613bd590c137ab6e80fd
jkachmar <jkachmar@groq.com>
2022-08-15 13:08:14 -0400

    Add nix cli config

---
+++ config/nix/default.nix
@@ -0,0 +1,19 @@
+{lib, ...}: let
+  # TODO: Abstract this out into its own module.
+  caches = [
+    # NixOS default cache.
+    {
+      url = "https://cache.nixos.org";
+      key = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+    }
+  ];
+  substituters = builtins.map (cache: cache.url) caches;
+  trustedPublicKeys = builtins.map (cache: cache.key) caches;
+in {
+  xdg.configFile."nix/nix.conf".text = ''
+    build-users-group = nixbld
+    experimental-features = nix-command flakes
+    substituters = ${lib.concatStringsSep " " substituters}
+    trusted-public-keys = ${lib.concatStringsSep " " trustedPublicKeys}
+  '';
+}
