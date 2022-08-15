39ac34b98052ab0136cfbae47f8427d11baf7d43
jkachmar <jkachmar@groq.com>
2022-08-15 13:52:29 -0700

    Add Linux config

--- flake.nix
+++ flake.nix
@@ -9,11 +9,19 @@
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "macosPkgs";
     };
+
+    nixosPkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixosHome = {
+      url = "github:nix-community/home-manager/release-22.05";
+      inputs.nixpkgs.follows = "nixosPkgs";
+    };
   };
 
   outputs = inputs @ {
     macosPkgs,
     macosHome,
+    nixosPkgs,
+    nixosHome,
     unstablePkgs,
     ...
   }: let
@@ -41,6 +49,26 @@
         homeDirectory = "/Users/jkachmar";
         stateVersion = "22.05";
         configuration.imports = [./hosts/manhattan-transfer];
+      };
+
+      # NOTE: $WORK configures my development VM automatically & assigns it a
+      # hostname based off of my username, BUT it's important to keep this
+      # config's naming scheme consistent (i.e. using JoJo stands).
+      jkachmar = nixosHome.lib.homeManagerConfiguration rec {
+        system = "x86_64-linux";
+        pkgs = mkPkgsFor system nixosPkgs;
+        extraModules = [
+          {
+            _module.args.inputs = inputs;
+            _module.args.unstable = mkPkgsFor system unstablePkgs;
+          }
+        ];
+
+        # NOTE: This changes pretty drastically in 22.11;
+        username = "jkachmar";
+        homeDirectory = "/Users/jkachmar";
+        stateVersion = "22.05";
+        configuration.imports = [./hosts/highway-star];
       };
     };
   };
