59e957ae4d6f06b72f1428266e0dd0ab3a123dbb
jkachmar <jkachmar@groq.com>
2022-08-22 12:21:59 -0400

    Adds workaround for macOS updates resetting /etc/zshrc

--- hosts/manhattan-transfer/default.nix
+++ hosts/manhattan-transfer/default.nix
@@ -4,4 +4,20 @@
     ../../config/fonts.nix
     ../../config/vscode
   ];
+
+  # NOTE: macOS version upgrades reset '/etc/zshrc', which means that the shell
+  # no longer "knows" how to source all Nix-related stuff.
+  #
+  # Hopefully this is a reasonable workaround, but if not then switching to
+  # 'fish' works in a pinch (i.e. how i've avoided running into this problem on
+  # other macOS machines so far.
+  #
+  # cf. https://discourse.nixos.org/t/nix-commands-missing-after-macos-12-1-version-upgrade/16679
+  programs.zsh.initExtraFirst = ''
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
+  '';
 }
