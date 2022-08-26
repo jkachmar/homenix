a636dba1931b39a265070fee2dfefba21b44e103
jkachmar <jkachmar@groq.com>
2022-08-26 13:36:42 -0700

    Adds podman configs for highway-star

--- hosts/highway-star/default.nix
+++ hosts/highway-star/default.nix
@@ -1,5 +1,16 @@
-{...}: {
+{config, ...}: let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in {
   imports = [
     ../../config
   ];
+
+  xdg.configFile = {
+    "containers/policy.json".source =
+      mkOutOfStoreSymlink
+        "${config.xdg.configHome}/dotfiles/hosts/highway-star/policy.json";
+    "containers/registries.conf".source =
+      mkOutOfStoreSymlink
+        "${config.xdg.configHome}/dotfiles/hosts/highway-star/registries.conf";
+  };
 }
