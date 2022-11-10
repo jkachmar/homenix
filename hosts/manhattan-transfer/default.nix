f4e090734430a71e79ea1a8853f30e16849eb81d
jkachmar <jkachmar@groq.com>
2022-11-10 12:03:02 -0500

    [macos] fixes fish path nonsese

--- hosts/manhattan-transfer/default.nix
+++ hosts/manhattan-transfer/default.nix
@@ -32,9 +32,14 @@ in {
     };
   };
 
-  # Fixes a bug where fish shell doesn't properly set up the nix path on macOS.
+  # Fixes a bug where fish shell doesn't properly set up the PATH on macOS.
+  #
+  # FIXME: Looks like a clean reinstall of Nix should fix this now that the
+  # installer provides a fish config.
+  #
+  # cf. https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1299764109
   programs.fish.shellInit = ''
-    for p in /run/current-system/sw/bin ~/.nix-profile/bin
+    for p in /usr/local/bin /nix/var/nix/profiles/default/bin ~/.nix-profile/bin
       if not contains $p $fish_user_paths
         set -g fish_user_paths $p $fish_user_paths
       end
